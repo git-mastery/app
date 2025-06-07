2822196cc11d9a738da7107e7aa85e63e2e75e99
Jiahao, Woo <woojiahao1234@gmail.com>
2025-06-07 15:45:53 +0800

    Add Git check command

--- cli.py
+++ cli.py
@@ -29,7 +29,7 @@ def error(message: str) -> None:
     click.echo(
         f"{click.style(' ERROR ', fg='black', bg='bright_red', bold=True)} {message}"
     )
-    exit()
+    exit(1)
 
 
 def info(message: str) -> None:
@@ -45,6 +45,12 @@ def debug(message: str) -> None:
 def warn(message: str) -> None:
     click.echo(
         f"{click.style(' WARN ', fg='black', bg='bright_yellow', bold=True)} {message}"
+    )
+
+
+def success(message: str) -> None:
+    click.echo(
+        f"{click.style(' SUCCESS ', fg='black', bg='bright_green', bold=True)} {message}"
     )
 
 
@@ -255,6 +261,30 @@ def read_gitmastery_exercise_config(gitmastery_exercise_config_path: Path) -> Di
         return json.loads(f.read())
 
 
+def is_git_installed(verbose: bool) -> bool:
+    result = subprocess.run(["git", "--version"], capture_output=True, text=True)
+    if verbose:
+        if result.returncode == 0:
+            print(result.stdout)
+        else:
+            print(result.stderr)
+    return result.returncode == 0
+
+
+def get_git_config(key: str, verbose: bool) -> Optional[str]:
+    result = subprocess.run(
+        ["git", "config", "--global", "--get", key], capture_output=True, text=True
+    )
+    if result.returncode == 0 and result.stdout.strip():
+        if verbose:
+            print(result.stdout)
+        return result.stdout.strip()
+    else:
+        if verbose:
+            print(result.stderr)
+        return None
+
+
 @click.group()
 @click.option("--verbose", "-v", is_flag=True, help="Enable verbose output")
 @click.pass_context
@@ -438,6 +468,51 @@ def verify(_ctx: click.Context) -> None:
     info(f"{click.style('Status:', bold=True)} {output.status}")
     info(click.style("Comments:", bold=True))
     print("\n".join([f"\t- {comment}" for comment in (output.comments or [])]))
+
+
+@cli.command()
+@click.argument("phase")
+@click.pass_context
+def check(ctx: click.Context, phase: str) -> None:
+    verbose = ctx.obj["VERBOSE"]
+    allowed = {"git", "github"}
+    phase = phase.strip().lower()
+    if phase not in allowed:
+        error("Select 'git' or 'github' for the phase to check")
+
+    info(f"Checking {phase}...")
+
+    if phase == "git":
+        info("Checking that you have Git installed and configured")
+        # TODO: verify that exit code is 1 on failure
+        if is_git_installed(verbose):
+            info("Git is installed")
+        else:
+            error("Git is not installed")
+
+        config_user_name = get_git_config("user.name", verbose)
+        if not config_user_name:
+            error(
+                f"You do not have {click.style('user.name', bold=True)} yet. Run {click.style('git config --global user.name <name>', bold=True, italic=True)}."
+            )
+        else:
+            info(
+                f"You have set {click.style('user.name', bold=True)} as {click.style(config_user_name, bold=True, italic=True)}"
+            )
+
+        config_user_email = get_git_config("user.email", verbose)
+        if not config_user_email:
+            error(
+                f"You do not have {click.style('user.email', bold=True)} yet. Run {click.style('git config --global user.email <email>', bold=True, italic=True)}."
+            )
+        else:
+            info(
+                f"You have set {click.style('user.email', bold=True)} as {click.style(config_user_email, bold=True, italic=True)}"
+            )
+
+        success("Git is installed and configured")
+    elif phase == "github":
+        info("Checking that you have Github CLI is installed and configured")
 
 
 if __name__ == "__main__":
