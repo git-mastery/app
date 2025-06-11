410d88415783f152e57cbc177cc8ee049bd2f45e
Jiahao, Woo <woojiahao1234@gmail.com>
2025-06-11 08:12:21 +0800

    Add check for Git and Github before download

--- cli/commands/download.py
+++ cli/commands/download.py
@@ -1,10 +1,12 @@
 import os
+import shutil
 import subprocess
 from pathlib import Path
 
 import click
 
-from cli.utils.click_utils import error, info
+from cli.commands.check import check
+from cli.utils.click_utils import error, info, success, warn
 from cli.utils.git_cli_utils import add_all, commit, init
 from cli.utils.gitmastery_utils import (
     download_file,
@@ -64,6 +66,36 @@ def download(ctx: click.Context, exercise: str) -> None:
         )
     config = read_gitmastery_exercise_config(Path("./"))
 
+    # Check if the exercise requires Git to operate, if so, error if not present
+    if config.get("requires_repo", True):
+        try:
+            info("Exercise requires Git, checking if you have it setup")
+            ctx.invoke(check, phase="git")
+        except SystemExit as e:
+            if e.code == 1:
+                # Exited because of missing Github configuration
+                # Rollback the download and remove the folder
+                warn("Git is not setup. Rolling back the download")
+                os.chdir("..")
+                shutil.rmtree(formatted_exercise)
+                warn("Setup Git before downloading this exercise")
+                exit(1)
+
+    # Check if the exercise requires Github/Github CLI to operate, if so, error if not present
+    if config.get("requires_github", False):
+        try:
+            info("Exercise requires Github, checking if you have it setup")
+            ctx.invoke(check, phase="github")
+        except SystemExit as e:
+            if e.code == 1:
+                # Exited because of missing Github configuration
+                # Rollback the download and remove the folder
+                warn("Github is not setup. Rolling back the download")
+                os.chdir("..")
+                shutil.rmtree(formatted_exercise)
+                warn("Setup Github and Github CLI before downloading this exercise")
+                exit(1)
+
     if len(config.get("resources", {})) > 0:
         info("Downloading resources...")
 
@@ -86,6 +118,6 @@ def download(ctx: click.Context, exercise: str) -> None:
     execute_py_file_function_from_url(
         formatted_exercise, "download.py", "setup", verbose=verbose
     )
-    info(f"Completed setting up {click.style(exercise, bold=True, italic=True)}")
+    success(f"Completed setting up {click.style(exercise, bold=True, italic=True)}")
     info("Start working on it:")
     info(click.style(f"cd {formatted_exercise}", bold=True, italic=True))
