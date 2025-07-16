b37bf3371640dca8b51e558f544f4686cc6c2a4c
Jiahao, Woo <woojiahao1234@gmail.com>
2025-07-16 13:04:14 +0800

    Adopt new command wrapper

--- app/utils/gh_cli_utils.py
+++ app/utils/gh_cli_utils.py
@@ -5,105 +5,64 @@ from typing import Dict, List, Literal, Optional
 
 from app.utils.cli_utils import get_stdout_stderr
 from app.utils.click_utils import error, info
+from app.utils.command_utils import run
 
 
 def is_github_cli_installed(verbose: bool) -> bool:
-    result = subprocess.run(["gh", "--version"], capture_output=True, text=True)
     # If git is not installed yet, we should expect a 127 exit code
     # 127 indicating that the command not found: https://stackoverflow.com/questions/1763156/127-return-code-from
-    if verbose:
-        if result.returncode == 0:
-            print(result.stdout)
-        else:
-            print(result.stderr)
-    return result.returncode == 0
+    result = run(["gh", "--version"], verbose)
+    return result.is_success()
 
 
 def get_https_or_ssh(verbose: bool) -> Optional[str]:
-    try:
-        result = subprocess.run(
-            ["gh", "auth", "status"],
-            check=True,
-            capture_output=True,
-            text=True,
-            env=dict(os.environ, **{"GH_PAGER": "cat"}),
-        )
-        if verbose:
-            print(result.stdout.strip())
+    result = run(["gh", "auth", "status"], verbose, {"GH_PAGER": "cat"})
+    if result.is_success():
         regex = re.compile(r"Git operations protocol: (\w+)")
-        match = regex.search(result.stdout.strip())
+        match = regex.search(result.stdout)
         if match:
             protocol = match.group(1).lower()
             return protocol
-        else:
-            return None
-    except subprocess.CalledProcessError as e:
-        if verbose:
-            print(e.stderr)
-        return None
+    return None
 
 
 def is_authenticated(verbose: bool) -> bool:
-    stdout, stderr = get_stdout_stderr(verbose)
-    return subprocess.call(["gh", "auth", "status"], stdout=stdout, stderr=stderr) == 0
+    return run(["gh", "auth", "status"], verbose).is_success()
 
 
 def has_fork(fork_name: str, verbose: bool) -> bool:
-    try:
-        result = subprocess.run(
-            ["gh", "repo", "view", fork_name, "--json", "isFork", "--jq", ".isFork"],
-            check=True,
-            capture_output=True,
-            env=dict(os.environ, **{"GH_PAGER": "cat"}),
-        )
-        if verbose:
-            print(result.stdout.strip())
-        return result.stdout.strip().decode("utf-8") == "true"
-    except subprocess.CalledProcessError as e:
-        if verbose:
-            print(e.stderr)
-        return False
+    result = run(
+        ["gh", "repo", "view", fork_name, "--json", "isFork", "--jq", ".isFork"],
+        verbose,
+        env={"GH_PAGER": "cat"},
+    )
+    return result.is_success() and result.stdout == "true"
 
 
 def get_repo_ssh_url(repo: str, verbose: bool) -> Optional[str]:
-    try:
-        result = subprocess.run(
-            ["gh", "repo", "view", repo, "--json", "sshUrl", "--jq", ".sshUrl"],
-            check=True,
-            capture_output=True,
-            text=True,
-            env=dict(os.environ, **{"GH_PAGER": "cat"}),
-        )
-        if verbose:
-            print(result.stdout.strip())
-        return result.stdout.strip()
-    except subprocess.CalledProcessError as e:
-        if verbose:
-            print(e.stderr)
-        return None
+    result = run(
+        ["gh", "repo", "view", repo, "--json", "sshUrl", "--jq", ".sshUrl"],
+        verbose,
+        env={"GH_PAGER": "cat"},
+    )
+    if result.is_success():
+        return result.stdout
+    return None
 
 
 def get_repo_https_url(repo: str, verbose: bool) -> Optional[str]:
-    try:
-        result = subprocess.run(
-            ["gh", "repo", "view", repo, "--json", "url", "--jq", ".url"],
-            check=True,
-            capture_output=True,
-            text=True,
-            env=dict(os.environ, **{"GH_PAGER": "cat"}),
-        )
-        if verbose:
-            print(result.stdout.strip())
-        return result.stdout.strip() + ".git"
-    except subprocess.CalledProcessError as e:
-        if verbose:
-            print(e.stderr)
-        return None
+    result = run(
+        ["gh", "repo", "view", repo, "--json", "url", "--jq", ".url"],
+        verbose,
+        env={"GH_PAGER": "cat"},
+    )
+    if result.is_success():
+        return result.stdout + ".git"
+    return None
 
 
 def fork(repository_name: str, fork_name: str, verbose: bool) -> None:
-    stdout, stderr = get_stdout_stderr(verbose)
-    subprocess.run(
+    run(
         [
             "gh",
             "repo",
@@ -113,37 +72,26 @@ def fork(repository_name: str, fork_name: str, verbose: bool) -> None:
             "--fork-name",
             fork_name,
         ],
-        stdout=stdout,
-        stderr=stderr,
+        verbose,
     )
 
 
 def clone(repository_name: str, verbose: bool) -> None:
-    stdout, stderr = get_stdout_stderr(verbose)
-    subprocess.run(
-        ["gh", "repo", "clone", repository_name], stdout=stdout, stderr=stderr
-    )
+    run(["gh", "repo", "clone", repository_name], verbose)
 
 
 def clone_with_custom_name(repository_name: str, name: str, verbose: bool) -> None:
-    stdout, stderr = get_stdout_stderr(verbose)
-    subprocess.run(
-        ["gh", "repo", "clone", repository_name, name], stdout=stdout, stderr=stderr
-    )
+    run(["gh", "repo", "clone", repository_name, name], verbose)
 
 
 def delete_repo(repository_name: str, verbose: bool) -> None:
-    stdout, stderr = get_stdout_stderr(verbose)
-    subprocess.run(
-        ["gh", "repo", "delete", repository_name, "--yes"], stdout=stdout, stderr=stderr
-    )
+    run(["gh", "repo", "delete", repository_name, "--yes"], verbose)
 
 
 def pull_request(
     repo: str, base: str, head: str, title: str, body: str, verbose: bool
 ) -> None:
-    stdout, stderr = get_stdout_stderr(verbose)
-    subprocess.run(
+    run(
         [
             "gh",
             "pr",
@@ -159,121 +107,91 @@ def pull_request(
             "--body",
             body,
         ],
-        stdout=stdout,
-        stderr=stderr,
+        verbose,
     )
 
 
 def get_prs(repo: str, head: str, owner: str, verbose: bool) -> List[str]:
-    try:
-        result = subprocess.run(
-            [
-                "gh",
-                "pr",
-                "list",
-                "--repo",
-                repo,
-                "--author",
-                "@me",
-                "--head",
-                head,
-                "--json",
-                "headRepositoryOwner",
-                "--json",
-                "url",
-                "-q",
-                f'.[] | select ( .headRepositoryOwner.login == "{owner}" ) | .url',
-            ],
-            capture_output=True,
-            text=True,
-            check=True,
-            env=dict(os.environ, **{"GH_PAGER": "cat"}),
-        )
-        prs = result.stdout.strip().splitlines()
-        if verbose:
-            info(", ".join(prs))
+    result = run(
+        [
+            "gh",
+            "pr",
+            "list",
+            "--repo",
+            repo,
+            "--author",
+            "@me",
+            "--head",
+            head,
+            "--json",
+            "headRepositoryOwner",
+            "--json",
+            "url",
+            "-q",
+            f'.[] | select ( .headRepositoryOwner.login == "{owner}" ) | .url',
+        ],
+        verbose,
+        env={"GH_PAGER": "cat"},
+    )
+
+    if result.is_success():
+        prs = result.stdout.splitlines()
         return prs
-    except subprocess.CalledProcessError as e:
-        if verbose:
-            error(e.stderr)
-        return []
+    return []
 
 
 def get_username(verbose: bool) -> str:
-    try:
-        result = subprocess.run(
-            ["gh", "api", "user", "-q", ".login"],
-            capture_output=True,
-            text=True,
-            check=True,
-        )
-        username = result.stdout.strip().splitlines()[0]
-        if verbose:
-            info(username)
+    result = run(["gh", "api", "user", "-q", ".login"], verbose)
+
+    if result.is_success():
+        username = result.stdout.splitlines()[0]
         return username
-    except subprocess.CalledProcessError as e:
-        if verbose:
-            error(e.stderr)
-        return ""
+    return ""
 
 
 def get_user_orgs(verbose: bool) -> List[str]:
-    try:
-        result = subprocess.run(
-            [
-                "gh",
-                "api",
-                "-H",
-                "Accept: application/vnd.github+json",
-                "/user/orgs",
-                "--jq",
-                ".[].login",
-                "--paginate",
-            ],
-            capture_output=True,
-            text=True,
-            check=True,
-        )
-        org_names = result.stdout.strip().splitlines()
-        if verbose:
-            info(", ".join(org_names))
+    result = run(
+        [
+            "gh",
+            "api",
+            "-H",
+            "Accept: application/vnd.github+json",
+            "/user/orgs",
+            "--jq",
+            ".[].login",
+            "--paginate",
+        ],
+        verbose,
+    )
+    if result.is_success():
+        org_names = result.stdout.splitlines()
         return org_names
-    except subprocess.CalledProcessError as e:
-        if verbose:
-            error(e.stderr)
-        return []
+    return []
 
 
 def get_user_prs(repo: str, owner: str, verbose: bool) -> List[str]:
-    try:
-        result = subprocess.run(
-            [
-                "gh",
-                "pr",
-                "list",
-                "--repo",
-                repo,
-                "--author",
-                "@me",
-                "--head",
-                "submission",
-                "--json",
-                "headRepositoryOwner",
-                "--json",
-                "url",
-                "-q",
-                f'.[] | select ( .headRepositoryOwner.login == "{owner}" ) | .url',
-            ],
-            capture_output=True,
-            text=True,
-            check=True,
-            env=dict(os.environ, **{"GH_PAGER": "cat"}),
-        )
-        prs = result.stdout.strip().splitlines()
-        if verbose:
-            info(", ".join(prs))
+    result = run(
+        [
+            "gh",
+            "pr",
+            "list",
+            "--repo",
+            repo,
+            "--author",
+            "@me",
+            "--head",
+            "submission",
+            "--json",
+            "headRepositoryOwner",
+            "--json",
+            "url",
+            "-q",
+            f'.[] | select ( .headRepositoryOwner.login == "{owner}" ) | .url',
+        ],
+        verbose,
+        env={"GH_PAGER": "cat"},
+    )
+    if result.is_success():
+        prs = result.stdout.splitlines()
         return prs
-    except subprocess.CalledProcessError as e:
-        if verbose:
-            error(e.stderr)
-        return []
+    return []
