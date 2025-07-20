13c33e3b9cb866eb133596379f65884a7239e5f9
Jiahao, Woo <woojiahao1234@gmail.com>
2025-07-20 09:28:30 +0800

    Show list of exercise progress regardless

--- app/commands/progress/show.py
+++ app/commands/progress/show.py
@@ -24,37 +24,42 @@ def show(ctx: click.Context) -> None:
     if not gitmastery_config.get("progress_local", False):
         error("You do not have progress tracking supported.")
 
+    if not os.path.isdir(gitmastery_root_path / LOCAL_FOLDER_NAME):
+        error(
+            f"Something strange has occurred, try to recreate the Git-Mastery exercise directory using {click.style('gitmastery setup', bold=True, italic=True)}"
+        )
+
+    if gitmastery_config.get("progress_remote", False):
+        ctx.invoke(github)
+
+    progress_file_path = gitmastery_root_path / LOCAL_FOLDER_NAME / "progress.json"
+    all_progress = []
+    if os.path.isfile(progress_file_path):
+        with open(progress_file_path, "r") as file:
+            all_progress = json.load(file)
+
+    all_progress.sort(
+        key=lambda entry: (entry["exercise_name"], -entry["completed_at"])
+    )
+    seen = set()
+    results = []
+    for i in range(len(all_progress)):
+        if all_progress[i]["exercise_name"] in seen:
+            continue
+        seen.add(all_progress[i]["exercise_name"])
+        results.append(
+            f"{all_progress[i]['exercise_name']}: {all_progress[i]['status']}"
+        )
+
     if gitmastery_config.get("progress_remote", False):
         ctx.invoke(github)
         username = get_username(verbose)
         dashboard_url = (
             f"https://git-mastery.github.io/progress-dashboard/#/dashboard/{username}"
         )
-        info(
+        results.append("")
+        results.append(
             f"Check out your progress on the dashboard: {click.style(dashboard_url, bold=True, italic=True)}"
         )
-    else:
-        info("Check out your progress below:")
-        if not os.path.isdir(gitmastery_root_path / LOCAL_FOLDER_NAME):
-            error(
-                f"Something strange has occurred, try to recreate the Git-Mastery exercise directory using {click.style('gitmastery setup', bold=True, italic=True)}"
-            )
-        progress_file_path = gitmastery_root_path / LOCAL_FOLDER_NAME / "progress.json"
-        all_progress = []
-        if os.path.isfile(progress_file_path):
-            with open(progress_file_path, "r") as file:
-                all_progress = json.load(file)
 
-        all_progress.sort(
-            key=lambda entry: (entry["exercise_name"], -entry["completed_at"])
-        )
-        seen = set()
-        results = []
-        for i in range(len(all_progress)):
-            if all_progress[i]["exercise_name"] in seen:
-                continue
-            seen.add(all_progress[i]["exercise_name"])
-            results.append(
-                f"{all_progress[i]['exercise_name']}: {all_progress[i]['status']}"
-            )
-        click.echo_via_pager("\n".join(results))
+    click.echo_via_pager("\n".join(results))
