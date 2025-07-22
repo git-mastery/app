fba3200db0da63da5004c7c795c2e2e636b6c064
Jiahao, Woo <woojiahao1234@gmail.com>
2025-07-22 08:11:09 +0800

    Remove clone on first setup

--- app/commands/setup.py
+++ app/commands/setup.py
@@ -4,10 +4,8 @@ import os
 import click
 
 from app.commands.check.git import git
-from app.commands.progress.constants import PROGRESS_REPOSITORY_NAME
+from app.commands.progress.constants import LOCAL_FOLDER_NAME
 from app.utils.click import error, info, prompt
-from app.utils.gh_cli import clone
-from app.utils.git_cli import remove_remote
 
 
 @click.command()
@@ -16,8 +14,6 @@ def setup(ctx: click.Context) -> None:
     """
     Sets up Git-Mastery for your local machine.
     """
-    verbose = ctx.obj["VERBOSE"]
-
     info(
         "Welcome to Git-Mastery! We will be setting up Git-Mastery for your local machine."
     )
@@ -39,14 +35,14 @@ def setup(ctx: click.Context) -> None:
     os.chdir(directory_path)
 
     info("Setting up your local progress tracker...")
-    clone(PROGRESS_REPOSITORY_NAME, verbose)
+    os.makedirs(LOCAL_FOLDER_NAME, exist_ok=True)
     with open(".gitmastery.json", "w") as gitmastery_file:
         gitmastery_file.write(
             json.dumps({"progress_local": True, "progress_remote": False})
         )
 
-    os.chdir(os.path.join(directory_path, "progress"))
-    remove_remote("origin", verbose)
+    with open("progress/progress.json", "a") as progress_file:
+        progress_file.write(json.dumps({}, indent=2))
 
     info(
         f"Setup complete. Your directory is: {click.style(directory_name, bold=True, italic=True)}"
