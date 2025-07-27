63c6da6c79b46afd8924c231c241d919728edcdf
Jiahao, Woo <woojiahao1234@gmail.com>
2025-07-27 16:57:23 +0800

    Add rmtree to force remove readonly folders

--- app/utils/cli.py
+++ app/utils/cli.py
@@ -1,3 +1,6 @@
+import os
+import shutil
+import stat
 import subprocess
 from typing import Optional, Tuple
 
@@ -6,3 +9,11 @@ def get_stdout_stderr(verbose: bool) -> Tuple[Optional[int], Optional[int]]:
     stdout = None if verbose else subprocess.DEVNULL
     stderr = None if verbose else subprocess.DEVNULL
     return stdout, stderr
+
+
+def rmtree(folder_name: str) -> None:
+    def force_remove_readonly(func, path, _):
+        os.chmod(path, stat.S_IWRITE)
+        func(path)
+
+    shutil.rmtree(folder_name, onerror=force_remove_readonly)
