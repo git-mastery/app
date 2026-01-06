1cf476b34252c7a8d28962b51055a9ff4e24270d
jovnc <95868357+jovnc@users.noreply.github.com>
2026-01-06 22:17:43 +0800

    Fix whitespace

--- app/commands/download.py
+++ app/commands/download.py
@@ -255,8 +255,8 @@ def setup_exercise_folder(
 
     info("Executing download setup")
     verbose = get_verbose()
-
     null_repo = not config.exercise_repo.init
+    
     with create_repo_smith(verbose, existing_path=".", null_repo=null_repo) as repo_smith:
         namespace.execute_function(
             "setup",
